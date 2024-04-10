7b772870204e4f3ea5842c3cc8a4db4c551f7cee
bluesimp1102 <jackismyname7@gmail.com>
2024-04-10 11:58:23 -0700

    add mysql db export and import scripts

---
+++ export-db.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Load environment variables from .env file
+set -o allexport
+source .env.export
+set +o allexport
+
+# Check if a database name was provided as a parameter
+if [ -z "$1" ]; then
+    echo "Please provide the database name as a parameter."
+    exit 1
+fi
+
+# Export the specified MySQL database
+mysqldump -h "$MYSQL_DB_HOST" -u "$MYSQL_DB_USERNAME" -p"$MYSQL_DB_PASSWORD" --port="$MYSQL_DB_PORT" "$1" > "$1".sql
+
+echo "Database '$1' exported to $1.sql"
---
+++ import-db.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Load environment variables from .env.import file
+set -o allexport
+source .env.import
+set +o allexport
+
+# Check if a database name was provided as a parameter
+if [ -z "$1" ]; then
+    echo "Please provide the database name as a parameter."
+    exit 1
+fi
+
+# Check if the SQL file exists
+if [ ! -f "$1.sql" ]; then
+    echo "The file '$1.sql' does not exist."
+    exit 1
+fi
+
+# Get the container ID of the MySQL container
+MYSQL_CONTAINER_ID=$(docker ps | grep mysql | awk '{print $1}')
+
+# Import the SQL file into the MySQL container
+docker exec -i "$MYSQL_CONTAINER_ID" mysql -u "$MYSQL_DB_USERNAME" -p"$MYSQL_DB_PASSWORD" "$MYSQL_DB_DATABASE" < "$1.sql"
+
+echo "Database '$1' imported successfully."
